019b608d88bca8f5f5be4cf11f9749a6c714c44b
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-01-13 21:34:30 +0530

    created router and controller for update and delete tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -40,9 +40,62 @@ const getUserTweets = asyncHandler(async(req, res)=> {
    .json(new ApiResponse(200, tweets, 'Tweets fetched successfully'));
 })
 
+
+const updateTweet = asyncHandler(async(req, res)=> {
+   const {tweetId} = req.params;
+   const {content} = req.body;
+
+   if(!content) {
+      throw new ApiError(400, 'Content is required');
+   }
+
+   const tweet = await Tweet.findById(tweetId);
+   if(!tweet) {
+      throw new ApiError(404, 'Tweet does not exist');
+   }
+
+   if(!tweet.owner.equals(req.user._id)) {
+      throw new ApiError(401, "This is someone else tweet you can't update");
+   }
+
+   const updatedTweet = await Tweet.findByIdAndUpdate(
+      tweetId,
+      {$set:{content}},
+      {new:true}
+   )
+
+   return res.status(200)
+   .json(new ApiResponse(200, updatedTweet, 'Tweet updated successfully'));
+})
+
+
+const deleteTweet = asyncHandler(async(req, res)=> {
+    const {tweetId} = req.params;
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet) {
+      throw new ApiError(404, 'Tweet does not exist');
+    }
+
+    if(!tweet.owner.equals(req.user._id)) {
+      throw new ApiError(401, "This is someone else tweet you can't delete");
+    }
+
+    const deletedTweet = await Tweet.findByIdAndDelete(
+      tweetId,
+      {new:true}
+    )
+
+    return res.status(200)
+    .json(new ApiResponse(200, deletedTweet, 'Tweet deleted successfully'));
+})
+
 export {
     createTweet,
-    getUserTweets
+    getUserTweets,
+    updateTweet,
+    deleteTweet
 }
 
 
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import {createTweet, getUserTweets} from '../controllers/tweet.controller.js';
+import {createTweet, getUserTweets, updateTweet, deleteTweet} from '../controllers/tweet.controller.js';
 import {varifyJwt} from '../middlewares/auth.middleware.js';
 
 router.post('/', varifyJwt, createTweet);
 router.get('/:userId', varifyJwt, getUserTweets);
+router.put('/:tweetId', varifyJwt, updateTweet);
+router.delete('/:tweetId', varifyJwt, deleteTweet);
+
 
 export default router;
